d1cbf93c2e040aba83787f92fa25003209e4b618
Danilo Palacio <114814797+Danilo-Palacio@users.noreply.github.com>
2023-07-18 00:44:09 -0300

    testes com service worker
    por mais que esteja sendo difícil utilizar os services workers, estou conseguindo ver avanços

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+import { testGoItem } from './script.js';
+
+let resultado1 = testGoItem();
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && tab.url.includes('https://ctn.sistematodos.com.br/paginas/filiado/EditarFiliado.'))  {
+      const notificationOptions = {
+        type: 'basic',
+        iconUrl: './images/icon128.png',
+        title: 'Analise do Filiado',
+        message: `Encontramos ${resultado1.prop1} problem${resultado1.prop2} no contrato.`
+      };
+  
+      chrome.notifications.create(notificationOptions);
+    }
+  });
+  
